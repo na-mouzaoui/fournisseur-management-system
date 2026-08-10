4762e8f5784224760709245914c5d330e06397bb
na-mouzaoui <mouzaoui.nazim@gmail.com>
2026-08-10 10:18:38 +0100

    feat : add delete alert

--- database/add_supplier_statuses.sql
+++
@@ -1,9 +0,0 @@
--- Ajout des statuts fournisseur manquants
-IF NOT EXISTS (SELECT 1 FROM statut WHERE libelle = N'actif')
-    INSERT INTO statut (libelle) VALUES (N'actif');
-
-IF NOT EXISTS (SELECT 1 FROM statut WHERE libelle = N'blacklisté')
-    INSERT INTO statut (libelle) VALUES (N'blacklisté');
-
--- Vérification
-SELECT id, libelle FROM statut ORDER BY id;
